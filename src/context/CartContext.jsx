9dd03d7326ab9ac75f7d4fa9c1f6b1b0286ad0b3
ava jadidi <a.jadidi@ymail.com>
2024-12-15 06:29:54 -0800

    modified don

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -11,6 +11,8 @@ const initialState = {
 const reducer = (state, action) => {
   console.log(action);
   console.log(state);
+
+
   switch (action.type) {
     case "ADD_ITEM":
       if (!state.selectedItems.find((item) => item.id === action.payload.id)) {
@@ -20,49 +22,50 @@ const reducer = (state, action) => {
         ...state,
         ...sumProuducts(state.selectedItems),
         chekout: false,
-      
       };
 
-    case "REMOVE_ITEM":
-      { const newSelectedItems = state.selectedItems.filter(
+    case "REMOVE_ITEM": {
+      const newSelectedItems = state.selectedItems.filter(
         (item) => item.id !== action.payload.id
       );
       return {
         ...state,
         selectedItems: [...newSelectedItems],
         ...sumProuducts(newSelectedItems),
-      }; }
-    case "INCREASE":
-      { const increaseindex = state.selectedItems.findIndex(
+      };
+    }
+    case "INCREASE": {
+      const increaseindex = state.selectedItems.findIndex(
         (item) => item.id === action.payload.id
       );
       state.selectedItems[increaseindex].quantity++;
       return {
         ...state,
         ...sumProuducts(state.selectedItems),
-      }; }
-      case "DECREASE":
-        { const decreaseIndex = state.selectedItems.findIndex(
-          (item) => item.id === action.payload.id
-        );
-        state.selectedItems[decreaseIndex].quantity--;
-        return {
-          ...state,
-          ...sumProuducts(state.selectedItems),
-        }; }
+      };
+    }
+    case "DECREASE": {
+      const decreaseIndex = state.selectedItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      state.selectedItems[decreaseIndex].quantity--;
+      return {
+        ...state,
+        ...sumProuducts(state.selectedItems),
+      };
+    }
 
-        case "CHECKOUT":
-            return{
-            selectedItems:[],
-            itemsCounter:0,
-            total:0,
-            checkout:true,
-        }
+    case "CHECKOUT":
+      return {
+        selectedItems: [],
+        itemsCounter: 0,
+        total: 0,
+        checkout: true,
+      };
 
     default:
       throw new Error("Invalid Action");
   }
-
 };
 
 const CartContext = createContext();
@@ -71,7 +74,7 @@ function CartProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <CartContext.Provider value={{ state: state, dispatch: dispatch }}>
+    <CartContext.Provider value={{  state, dispatch }}>
       {children}
     </CartContext.Provider>
   );
